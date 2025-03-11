33e16bad4ad92b511ed35b466f1c982999bf4015
IAmToolbox <danielalejandro.leivafunes9@gmail.com>
2025-03-10 23:02:37 -0500

    Finalized game. Added ability to destroy asteroids. Project over.

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ from player import *
 from asteroid import *
 from asteroidfield import AsteroidField
 from shot import Shot
-
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+asteroids = pygame.sprite.Group()
+shots = pygame.sprite.Group()
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-    updatable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
-    asteroids = pygame.sprite.Group()
-    shots = pygame.sprite.Group()
+    
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
     Shot.containers = (updatable, drawable)
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, shots, drawable, updatable)
     field = AsteroidField()
     while True:
         for event in pygame.event.get():
@@ -32,12 +32,32 @@ def main():
             object.draw(screen)
         dt = clock.tick(60) / 1000
         updatable.update(dt)
+        print(asteroids)
+        print(shots)
+
+        asteroids_to_kill = []
+        bullets_to_kill = []
         for asteroid in asteroids:
             if asteroid.collission(player) == True:
                 print("Game Over!")
                 sys.exit()
+            for bullet in shots:
+                if asteroid.collission(bullet) == True:
+                    asteroids_to_kill.append(asteroid)
+                    bullets_to_kill.append(bullet) # pLEASE WORK
+        for asteroid in asteroids_to_kill:
+            asteroid.split()
+        for bullet in bullets_to_kill:
+            bullet.kill()
+        #for asteroid in asteroids:
+            #for bullet in shots:
+                #print("Checking for collission...")
+                #if asteroid.collission(bullet) == True:
+                    #asteroid.kill()
+                    #bullet.kill()
+                    #print("This should destroy the asteroid and the bullet")
         pygame.display.flip()
-        print(dt)
+        #print(dt)
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -5,9 +5,14 @@ from constants import *
 from shot import Shot
 
 class Player(CircleShape):
-    def __init__(self, x, y):
+    def __init__(self, x, y, shots_group, drawable_group, updatable_group):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_timer = 0
+
+        self.shots_group = shots_group
+        self.drawable_group = drawable_group
+        self.updatable_group = updatable_group
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -28,8 +33,14 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
     
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
-        shot.velocity = (pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOT_SPEED)
+        if self.shot_timer <= 0:
+            shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+            shot.velocity = (pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOT_SPEED)
+            self.shot_timer = PLAYER_SHOT_COOLDOWN
+
+            self.shots_group.add(shot)
+            self.drawable_group.add(shot)
+            self.updatable_group.add(shot)
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -44,5 +55,7 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
+        
+        self.shot_timer -= dt
     
     
